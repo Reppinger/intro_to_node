5918e9d31fd17328fd3801549b1806a8bc30d196
Randy Eppinger <Randy.Eppinger@leandog.com>
2015-05-20 11:33:54 -0400

    Pulled out reading of filename input parameter into function. Added exit when error occurs reading file.

--- ex1/ex1.js
+++ ex1/ex1.js
@@ -1,16 +1,19 @@
 var fs = require('fs');
 
+function get_file_parameter() {
+	return process.argv[2];
+}
 
 function print_file_contents(filename) {
 	fs.readFile(filename, {encoding: 'utf-8'},
 		function(err,file_contents) {
 			if(err) {
 				process.stdout.write(err);
+				process.exit;
 			}
 			process.stdout.write(file_contents); 
 		});
-};
-
+}
 
 function read_filename() {
 	var filename = "";
@@ -24,9 +27,9 @@ function read_filename() {
 		rl.close();
     });
     return filename;
-};
+}
 
-var file_parameter = process.argv[2];
+var file_parameter = get_file_parameter();
 if(file_parameter) {
 	print_file_contents(file_parameter);
 } else {
